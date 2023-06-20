27141f18b6bc3b96b0226a12e0fc8e017ed76150
Dion VH <91065539+pgm-dionvanh@users.noreply.github.com>
2023-06-20 15:17:54 +0200

    Netlify

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/netlify";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 export const handler = createRequestHandler({
   build,
